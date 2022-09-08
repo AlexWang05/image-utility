39fa9b03b893beff6b04887a1287cbfa4cae9a30
alexwang05 <hpwzz20@gmail.com>
2022-09-08 01:52:29 -0700

    working & tested dicom

--- FormatConverter.py
+++ FormatConverter.py
@@ -18,38 +18,40 @@ class FormatConverter:
         for filename in os.listdir(self.input_folder):
             # check if file is a DICOM file
             if filename.endswith('.dcm'):
+                new_name = filename.replace('.dcm', '.png')
+                
                 dicom_file = open(os.path.join(self.input_folder, filename), 'rb')
-                png_file = open(os.path.join(self.output_folder, filename), 'wb')
-
-                # mri_to_png(mri_file, png_file)
+                png_file = open(os.path.join(self.output_folder, new_name), 'wb')
                 
                 # Extracting data from the mri file
-                # plan = pydicom.read_file(dicom_file)
-                # shape = plan.pixel_array.shape
+                plan = pydicom.read_file(dicom_file)
+                shape = plan.pixel_array.shape
 
-                # image_2d = []
-                # max_val = 0
-                # for row in plan.pixel_array:
-                #     pixels = []
-                #     for col in row:
-                #         pixels.append(col)
-                #         if col > max_val: max_val = col
-                #     image_2d.append(pixels)
+                image_2d = []
+                max_val = 0
+                for row in plan.pixel_array:
+                    pixels = []
+                    for col in row:
+                        pixels.append(col)
+                        if col > max_val: max_val = col
+                    image_2d.append(pixels)
 
-                # # Rescaling grey scale between 0-255
-                # image_2d_scaled = []
-                # for row in image_2d:
-                #     row_scaled = []
-                #     for col in row:
-                #         col_scaled = int((float(col) / float(max_val)) * 255.0)
-                #         row_scaled.append(col_scaled)
-                #     image_2d_scaled.append(row_scaled)
+                # Rescaling grey scale between 0-255
+                image_2d_scaled = []
+                for row in image_2d:
+                    row_scaled = []
+                    for col in row:
+                        col_scaled = int((float(col) / float(max_val)) * 255.0)
+                        row_scaled.append(col_scaled)
+                    image_2d_scaled.append(row_scaled)
 
-                # # Writing the PNG file
-                # w = png.Writer(shape[1], shape[0], greyscale=True)
-                # w.write(png_file, image_2d_scaled)
+                # Writing the PNG file
+                w = png.Writer(shape[1], shape[0], greyscale=True)
+                w.write(png_file, image_2d_scaled)
 
-                # png_file.close()
+                print('Converted ' + filename + ' to ' + new_name)
+
+                png_file.close()
 
 
 
